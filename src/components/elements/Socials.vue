594c6020f6a568b7f46ec3dbfe1413eb73947236
Francesco <scovascova@gmail.com>
2022-01-11 18:56:40 +0100

    icone social

---
+++ src/components/elements/Socials.vue
@@ -0,0 +1,27 @@
+<template>
+  <ul>
+      <li><a href="#"><i class="fab fa-facebook-square primary-color"></i></a></li>
+      <li><a href="#"><i class="fab fa-twitter-square primary-color"></i></a></li>
+      <li><a href="#"><i class="fab fa-instagram-square primary-color"></i></a></li>
+      <li><a href="#"><i class="fab fa-youtube-square primary-color"></i></a></li>
+  </ul>
+</template>
+
+<script>
+export default {
+    name: 'Socials'
+}
+</script>
+
+<style lang="scss">
+    ul{
+        font-size: 20pt;
+        list-style: none;
+        display: flex;
+    }
+    li{
+        margin: 5px;
+        padding: 6px;
+    }
+    
+</style>
--- src/components/macro/Header.vue
+++ src/components/macro/Header.vue
@@ -1,15 +1,36 @@
 <template>
   <header>
-      
+      <div class="header-sup">
+          <div class="container">
+              <div id="announce">
+                <span>Avada & Associates legal team announce deal to open up oil & gas</span>
+              </div>
+              <div class="socials-container">
+                  <Socials/>
+              </div>
+          </div>
+          
+      </div>
   </header>
 </template>
 
 <script>
+import Socials from '../elements/Socials.vue';
+
 export default {
-    name: 'Header'
+    name: 'Header',
+    components: {
+        Socials
+    }
 }
 </script>
 
 <style lang="scss" scoped>
-
+    .container{
+        display: flex;
+    }
+    
+    i{
+        font-size: 30pt;
+    }
 </style>
