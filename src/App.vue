20ffdb7dd4be9a43c0d9f48b7f22960a294b8331
Francesco <scovascova@gmail.com>
2022-01-11 12:51:34 +0100

    Header importato in App

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    
+    <Header/>
   </div>
 </template>
 
 <script>
-
+import Header from './components/macro/Header.vue';
 
 export default {
   name: 'App',
   components: {
-    
+    Header
   }
 }
 </script>
